02819389e245f832404b335b41f195680d8773af
Hossein <hos.ghah@gmail.com>
2021-03-07 00:05:07 +0330

    validations and error messages added to index page form

--- javascripts/pages/index.js
+++ javascripts/pages/index.js
@@ -1,5 +1,45 @@
 // Example...
 // Avoid using jquery if possible.
 $(document).ready(() => {
-  console.log('Index page');
+  const token = document.querySelector("input[name='token']");
+  const url = document.querySelector("input[name='url']");
+  const form = document.getElementById("getProduct");
+  const error = document.getElementById("error");
+
+  document.querySelector("input[type='submit']").addEventListener("click", evt => {
+    evt.preventDefault();
+    evt.target.disabled = true;
+
+    let errors = document.createElement("ul");
+    errors.setAttribute("class", "error");
+    if(!tokenValidator(token.value)) {
+      const elem = document.createElement("li");
+      elem.innerText = "Invalid token!";
+      errors.appendChild(elem);
+    }
+    if(!urlValidator(url.value)) {
+      const elem = document.createElement("li");
+      elem.innerText = "Invalid url!";
+      errors.appendChild(elem);
+      evt.target.disabled = false;
+    }
+
+    if(errors.children.length > 0) {
+      error.innerHTML = "";
+      error.append(errors);
+    } else {
+      error.innerHTML = "";
+      form.submit();
+    }
+  })
 });
+
+function tokenValidator(token) {
+  return Boolean(token && token !== "");
+}
+
+function urlValidator(url) {
+  const pattern = /^https:\/\/www.amazon.com\//g;
+
+  return pattern.test(url);
+}
